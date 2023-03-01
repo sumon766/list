ddc80558f910aaf089721649f7beefda145d343e
Sumon <sumon766@gmail.com>
2023-03-01 22:59:47 +0600

    Fix linters errors

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,33 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
-    //
-    mode: 'development',
-    entry: './src/index.js',
+  //
+  mode: 'development',
+  entry: './src/index.js',
 
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, "src", "index.html")
-        }),
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src', 'index.html'),
+    }),
+  ],
+
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+
+  devServer: {
+    watchFiles: ['src/*.html'],
+    hot: true,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
     ],
-
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-
-    devServer: {
-        watchFiles: ["src/*.html"],
-        hot: true,
-    },
-
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
-        ],
-    },
-  };
+  },
+};
