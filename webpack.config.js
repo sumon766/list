bc933e1647cbb93fd808b9dc80e51be4c3295f7c
Sumon <sumon766@gmail.com>
2023-03-03 19:07:26 +0600

    Add CRUD functionality to To Do List

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,40 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  //
   mode: 'development',
-  entry: './src/index.js',
-
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'src', 'index.html'),
-    }),
-  ],
-
+  entry: {
+    index: path.resolve(__dirname, './src/index.js'),
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
-
-  devServer: {
-    watchFiles: ['src/*.html'],
-    hot: true,
+  optimization: {
+    runtimeChunk: 'single',
   },
-
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/index.html'),
+    }),
+  ],
   module: {
     rules: [
       {
-        test: /\.css$/i,
+        test: /\.css$/,
         use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
       },
     ],
   },
+
 };
