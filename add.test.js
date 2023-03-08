6a89b8cb74a3681956f54dfba05dc0849192441a
Sumon <sumon766@gmail.com>
2023-03-08 17:59:28 +0600

    To Do List Add & Remove Tests

--- add.test.js
+++
@@ -1,6 +0,0 @@
-const { default: ListCollection } = require('./modules/ListCollection');
-
-const data = new ListCollection();
-test ('Test the add item function to To-Do List', () => {
-    expect(data.setLocalData()).toBe();
-});
