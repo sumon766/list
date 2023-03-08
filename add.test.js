e602b5bd4466c2d545bf42920703862dbc79d8d8
Sumon <sumon766@gmail.com>
2023-03-08 16:10:08 +0600

    Jest Test File Add

---
+++ add.test.js
@@ -0,0 +1,6 @@
+const { default: ListCollection } = require('./modules/ListCollection');
+
+const data = new ListCollection();
+test ('Test the add item function to To-Do List', () => {
+    expect(data.setLocalData()).toBe();
+});
